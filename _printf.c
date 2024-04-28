930e8ff8fd2044ea31f7b697e443406f8a13cf9d
AndrewMuthumbi <joeandrews176@gmail.com>
2024-04-28 15:00:21 -0700

    TASK 1; updated _printf function to handle the %d and %i conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,47 +1,11 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-
-/**
- * print_char - Print a single character to stdout
- * @c: Character to print
- * @count: Pointer to the counter for characters printed
- */
-void print_char(char c, int *count)
-{
-write(1, &c, 1);
-(*count)++;
-}
-
-/**
- * print_string - Print a string to stdout
- * @str: String to print
- * @count: Pointer to the counter for characters printed
- */
-void print_string(char *str, int *count)
-{
-while (*str)
-{
-write(1, str, 1);
-str++;
-(*count)++;
-}
-}
-
-/**
- * print_percent - Print a literal '%' to stdout
- * @count: Pointer to the counter for characters printed
- */
-void print_percent(int *count)
-{
-write(1, "%", 1);
-(*count)++;
-}
+#include <stdio.h>
 
 /**
  * _printf - Print formatted output to stdout
  * @format: Format string
- *
  * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
@@ -54,17 +18,21 @@ va_start(args, format);
 while (*format)
 {
 if (*format != '%')
-print_char(*format, &count);
+{
+write(1, format, 1);
+count++;
+}
 else
 {
 format++;
-if (*format == 'c')
-print_char(va_arg(args, int), &count);
-
-else if (*format == 's')
-print_string(va_arg(args, char *), &count);
-else if (*format == '%')
-print_percent(&count);
+if (*format == 'd' || *format == 'i')
+{
+int num = va_arg(args, int);
+char buffer[20]; 
+int length = snprintf(buffer, sizeof(buffer), "%d", num);
+write(1, buffer, length);
+count += length;
+}
 }
 format++;
 }
